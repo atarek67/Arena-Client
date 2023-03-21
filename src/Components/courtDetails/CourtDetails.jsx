481b9fd96b933a85ac97627a95db0d629d4ed8a6
Ahmed Tarek <58385791+atarek67@users.noreply.github.com>
2023-03-21 17:15:11 +0200

    Client Side

---
+++ src/Components/courtDetails/CourtDetails.jsx
@@ -0,0 +1,56 @@
+
+import React, { useState } from "react";
+import { useLoaderData } from "react-router";
+import style from "./courtDetails.module.css";
+import axios from "axios";
+import ReservationForm from "./ReservationForm";
+import Carousel from "react-bootstrap/Carousel";
+import { useTranslation } from 'react-i18next';
+export default function CourtDetails() {
+  const image = "http://localhost:7500/api/images/fieldPic/";
+  const [t, i18n] = useTranslation();
+  let field = useLoaderData();
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+
+  return (
+    <>
+      <div className={style.All}>
+        {/* Carousel Section */}
+        <Carousel activeIndex={index} onSelect={handleSelect} className="m-lg-5 m-1">
+          {field.images?.map((i) => (
+            <Carousel.Item>
+              <img
+                className="d-block w-100 b-danger img-fluid d-lg-block d-none"
+                src={`${image}/${i}`}
+                alt="First slide"
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+
+        <div className={style.cardBody}>
+          <h4 className={style.cardTitle}>{t("Court Name")}: {field.fieldName}</h4>
+          <br />
+          <h5 className={style.cardTitle}>{t("Location")}: {field.location}</h5>
+          <br />
+          <h5 className={style.cardTitle}>{t("Price/Hour")}: {field.price} {t("EGP")}</h5>
+        </div>
+
+        <ReservationForm fieldId={field._id} fieldName={field.fieldName} price={field.price}></ReservationForm>
+      </div>
+    </>
+  );
+}
+
+export async function courtLoader({ params }) {
+  let { data } = await axios.get(
+    `http://localhost:7500/api/fields/${params.fieldID}`
+  );
+
+  return data;
+}
